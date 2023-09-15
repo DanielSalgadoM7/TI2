c67aeb36bb03c603a24a21548cc4865c8cee1910
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-09-15 04:06:38 -0300

    Update dump.sql

--- WS3/src/main/resources/dump.sql
+++ WS3/src/main/resources/dump.sql
@@ -1,10 +1,4 @@
--- Tabela de itens da loja do RPG
-CREATE TABLE public.loja (
-    id integer DEFAULT nextval('public."id-item"'::regclass) NOT NULL,
-    nome text,
-    efeito text,
-    preco float
-);
+--Daniel Salgado Magalhães
 
 -- Definindo a chave primária
 ALTER TABLE public.item ADD CONSTRAINT item_pkey PRIMARY KEY (id);
@@ -20,4 +14,14 @@ CREATE SEQUENCE public."id-item"
 -- Define a coluna 'id' para usar a sequência
 ALTER TABLE public.item ALTER COLUMN id SET DEFAULT nextval('public."id-item"'::regclass);
 
-ALTER TABLE public."id-item" OWNER TO ti2cc;
+ALTER TABLE public."id-item" OWNER TO WS3TI2;
+
+-- Tabela de itens da loja do RPG
+CREATE TABLE public.loja (
+    id integer DEFAULT nextval('public."id-item"'::regclass) NOT NULL,
+    nome text,
+    efeito text,
+    preco float
+);
+
+ALTER TABLE public.produto OWNER TO WS3TI2;
